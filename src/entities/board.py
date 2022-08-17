1c9673821a72dacae5d72203a4df7e4b261e892b
Roberto Betini Junior <55061623+robertobetini@users.noreply.github.com>
2022-08-17 02:59:18 -0300

    feat: implement piece display.
    Co-authored-by: Roberto <betini.roberto@gmail.com>

--- src/entities/board.py
+++ src/entities/board.py
@@ -1,14 +1,23 @@
+from entities.square_piece import SquarePiece
+
+from .piece import Piece
+
 class Board:
     def __init__(self, window, width=10, height=20):
         self.window = window
         self.width = width
         self.height = height
-        self.pieces = []
+        self.pieces = [SquarePiece(window, 2, 2)]
 
     def display(self):
-        self.window.addstr(1, 1, "+----------+")
+        self.window.addstr(1, 1, f"+{ self.width * '-' }+")
 
         for i in range(self.height):
-            self.window.addstr(2 + i, 1, "|          |")
+            self.window.addstr(2 + i, 1, f"|{ self.width * ' ' }|")
 
-        self.window.addstr(2 + self.height, 1, "+----------+")
+        self.window.addstr(2 + self.height, 1, f"+{ self.width * '-' }+")
+
+        for piece in self.pieces:
+            piece.display()
+
+    
--- src/entities/piece.py
+++ src/entities/piece.py
@@ -1,6 +1,32 @@
+from .math.point import Point
+
 class Piece:
-    def __init__(self, window):
+    PIECE_CENTER = 2
+    SOLID_BLOCK = 1
+    EMPTY_BLOCK = 0
+    PIECE_CHAR = "x"
+
+    def __init__(self, window, x_offset, y_offset):
         self.window = window
+        self.__x_offset = x_offset
+        self.__y_offset = y_offset
+        self.position = Point(x_offset, y_offset)
+        self.area = [
+            [self.PIECE_CENTER]
+        ]
 
     def display(self):
-        pass
+        self.window.addstr(self.position.y, self.position.x, self.PIECE_CHAR)
+
+        for i in range(len(self.area)):
+            for j in range(len(self.area[i])):
+                block = self.area[i][j]
+
+                if self.__is_solid(block):
+                    self.window.addstr(self.position.y + i, self.position.x + j, self.PIECE_CHAR)
+
+    def __is_solid(self, block):
+        if block == self.PIECE_CENTER or block == self.SOLID_BLOCK:
+            return True
+
+        return False
